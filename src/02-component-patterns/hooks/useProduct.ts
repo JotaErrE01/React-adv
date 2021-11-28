1bd02045edc5d6d7625d3cfaaf562a09c662a5f3
Jonathan Ruiz <jruiz.967@gmail.com>
2021-11-28 16:35:43 -0500

    Control props alternativo

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -10,15 +10,8 @@ interface useProductArgs {
 export const useProduct = ({ product, onChange, value = 0 } : useProductArgs) => {
 
     const [counter, setCounter] = useState(value);
-
-    const isControlled = useRef( !!onChange );
-    
+        
     const increaseBy = (value: number) => {
-
-        if(isControlled.current){
-            return onChange!({ count: value, product });
-        }        
-
         const newCount = Math.max(0, counter + value);
         setCounter( newCount );
         onChange && onChange({ product, count: newCount });
--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -10,31 +10,13 @@ export const useShoppingCart = () => {
     const [shoppingCart, setShoppingCart] = useState<{ [key: string]: ProductInCart }>({});
 
     const onProductCountChange = ({ product, count }: OnChangeArgs) => {
+        if(count === 0){
+          const { [product.id]: deletedElement, ...rest } = shoppingCart; 
+          setShoppingCart(rest);
+          return;
+        }
 
-        setShoppingCart(prevState => {
-
-            const productInCart: ProductInCart = prevState[product.id] || { ...product, count: 0 };
-
-            if (Math.max(productInCart.count + count, 0) > 0) {
-                productInCart.count += count;
-                return {
-                    ...prevState,
-                    [product.id]: productInCart,
-                }
-            }
-
-            // Borrar el producto
-            const { [product.id]: _, ...rest } = prevState;
-            return rest;
-        });
-
-        // if(count === 0){
-        //   const { [product.id]: deletedElement, ...rest } = shoppingCart; 
-        //   setShoppingCart(rest);
-        //   return;
-        // }
-
-        // setShoppingCart({ ...shoppingCart, [product.id]: { ...product, count } });
+        setShoppingCart({ ...shoppingCart, [product.id]: { ...product, count } });
     }
 
     return {
